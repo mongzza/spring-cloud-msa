16208342834df6381d13fc0dabddd767881ce8a3
dami <fdfdsa123@hanmail.net>
2022-03-03 01:25:43 +0900

    feat: add gateway sample
    - FirstService와 SecondService에 welcome API 추가
    - gateway 설정

---
+++ second-service/src/main/java/com/dami/secondservice/SecondServiceController.java
@@ -0,0 +1,16 @@
+package com.dami.secondservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/second-service")
+public class SecondServiceController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second service.";
+    }
+}
