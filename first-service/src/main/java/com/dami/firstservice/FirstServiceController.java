c3e92f6da1a2aad03ad41012d5e9295a892d178a
dami <fdfdsa123@hanmail.net>
2022-03-05 21:50:36 +0900

    feat: add load balancer with eureka
    - 로드밸런싱할 수 있도록 apigateway-service 설정 변경
    - first-service를 랜덤 포트로 띄울 수 있도록 설정 변경
    - 랜덤으로 띄울 때 어떤 포트로 접근했는지 확인할 수 있는 로깅 추가

--- first-service/src/main/java/com/dami/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/dami/firstservice/FirstServiceController.java
@@ -1,14 +1,22 @@
 package com.dami.firstservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+    
+    private final Environment env;
     
     @GetMapping("/welcome")
     public String welcome() {
@@ -16,7 +24,9 @@ public class FirstServiceController {
     }
     
     @GetMapping("/check")
-    public String check() {
-        return "This is CustomFilter of First service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        // random port 사용할 때는 프로퍼티명 local.server.port로 포트번호 확인 가능
+        return String.format("This is CustomFilter of First service on PORT %s", env.getProperty("local.server.port"));
     }
 }
