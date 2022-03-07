2166d3fe0fcb1ff1dd82f56d0fcd392815453733
dami <fdfdsa123@hanmail.net>
2022-03-07 21:22:37 +0900

    feat: user service link to apigateway service

--- user-service/src/main/java/com/dami/userservice/controller/HealthCheckController.java
+++ user-service/src/main/java/com/dami/userservice/controller/HealthCheckController.java
@@ -1,13 +1,20 @@
 package com.dami.userservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/user-service")
 public class HealthCheckController {
+    
+    private final Environment env;
     
     @GetMapping("/health-check")
     public String status() {
-        return "It's Working in User service.";
+        return String.format("It's Working in User service on PORT %s", env.getProperty("local.server.port"));
     }
 }
