198d0cba5551e6486f0fd6f8a8efdf1bb477eee6
dami <fdfdsa123@hanmail.net>
2022-03-10 16:48:30 +0900

    feat: create order-service

---
+++ order-service/src/main/java/com/dami/orderservice/OrderServiceApplication.java
@@ -0,0 +1,17 @@
+package com.dami.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderServiceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+    
+}
